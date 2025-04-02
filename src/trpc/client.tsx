9ab23c4fb60e7e38fbe73ec4f7025c596705c177
Shubham Patil <shubhamspatilnbr@gmail.com>
2025-04-02 20:27:19 +0530

    add TRPC configuration

--- src/trpc/client.tsx
+++ src/trpc/client.tsx
@@ -8,6 +8,7 @@ import { createTRPCReact } from "@trpc/react-query";
 import { useState } from "react";
 import { makeQueryClient } from "./query-client";
 import type { AppRouter } from "./routers/_app";
+import superjson from "superjson";
 
 export const trpc = createTRPCReact<AppRouter>();
 
@@ -45,8 +46,13 @@ export function TRPCProvider(
     trpc.createClient({
       links: [
         httpBatchLink({
-          // transformer: superjson, <-- if you use a data transformer
+          transformer: superjson, // <-- if you use a data transformer
           url: getUrl(),
+          async headers() {
+            const headers = new Headers();
+            headers.set("x-trpc-source", "nextjs-react");
+            return headers;
+          },
         }),
       ],
     })
